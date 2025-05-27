3c4f2b72a879ee4206e26dd5a1efbe8faf3e5064
elton <elton6103@gmail.com>
2025-05-27 20:02:42 -0300

    ♻️ refactor: rename to app.js and implement routes

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const login = require('./routes/login')
+const register = require('./routes/register')
+
+app.use(login)
+app.use(register)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
