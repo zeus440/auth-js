7857252f0911ba35907878590893f1de57bdbd02
elton <elton6103@gmail.com>
2025-05-27 20:00:40 -0300

    ✨ feat: add registration route

---
+++ routes/register.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+const users = [];
+
+router.post("/register", (req, res) => {
+  const { email, password } = req.body;
+
+  // Email Already Exist?
+  const emailAlreadyExists = users.some((user) => user.email === email);
+
+  if (emailAlreadyExists) {
+    return res.status(400).json({ error: "Email has been registred." });
+  }
+
+  res.json(req.body);
+
+  users.push({
+    email,
+    password,
+  });
+
+  return res.status(201).json({ message: "User created successfully! 🎉" });
+});
+
+module.exports = router;
