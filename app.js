f904ae1956bcddfee3e3376157c30c3a60662a3e
elton <elton6103@gmail.com>
2025-05-28 08:02:50 -0300

    ✨ feat: enhance route handling with rate limiters for login and register

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const helmet = require('helmet')
+const helmet = require("helmet");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 const login = require("./routes/login");
 const register = require("./routes/register");
-const profile = require("./routes/profile")
+const profile = require("./routes/profile");
 const { connectToDatabase } = require("./database/db");
+const {
+  loginLimiter,
+  registerLimiter,
+} = require("./middlewares/rateLimit.middleware");
 
-app.use(helmet())
-app.use(login);
-app.use(register);
-app.use(profile)
+app.use(helmet());
+app.use("/login", loginLimiter, login);
+app.use("/register", registerLimiter, register);
+app.use(profile);
 
 connectToDatabase().then(() => {
   app.listen(port, () => {
