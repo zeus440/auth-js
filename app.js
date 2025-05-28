d5e77eddd69855487d937ed52d272ec901653f1c
elton <elton6103@gmail.com>
2025-05-27 23:18:34 -0300

    ✨ feat: add helmet middleware for enhanced security

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const helmet = require('helmet')
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -6,6 +7,7 @@ const login = require("./routes/login");
 const register = require("./routes/register");
 const { connectToDatabase } = require("./database/db");
 
+app.use(helmet())
 app.use(login);
 app.use(register);
 
